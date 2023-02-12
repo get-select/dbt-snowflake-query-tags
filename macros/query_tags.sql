1564eaa75b17e072eef6a8fbccca8e7c668abbad
Rob Scriva <robscriva@canva.com>
2023-02-12 11:39:53 +1030

    Update query_tags.sql

--- macros/query_tags.sql
+++ macros/query_tags.sql
@@ -1,4 +1,8 @@
 {% macro set_query_tag() -%}
+  {{ return(adapter.dispatch('set_query_tag', 'common')()) }}
+{%- endmacro %}
+
+{% macro default__set_query_tag() -%}
     {# Start with any model-configured dict #}
     {% set tag_dict = config.get('query_tag', default={}) %}
 
@@ -62,14 +66,4 @@
     {{ log("Setting query_tag to '" ~ new_query_tag ~ "'. Will reset to '" ~ original_query_tag ~ "' after materialization.") }}
     {% do run_query("alter session set query_tag = '{}'".format(new_query_tag)) %}
     {{ return(original_query_tag)}}
-{% endmacro %}
-
-{% macro unset_query_tag(original_query_tag) -%}
-    {% if original_query_tag %}
-    {{ log("Resetting query_tag to '" ~ original_query_tag ~ "'.") }}
-    {% do run_query("alter session set query_tag = '{}'".format(original_query_tag)) %}
-    {% else %}
-    {{ log("No original query_tag, unsetting parameter.") }}
-    {% do run_query("alter session unset query_tag") %}
-    {% endif %}
 {% endmacro %}
