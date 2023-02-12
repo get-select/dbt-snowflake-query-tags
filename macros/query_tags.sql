8b4b29cde2db00a523784b7409dd19dd6e2060a0
Rob Scriva <robscriva@canva.com>
2023-02-13 10:15:31 +1030

    add back unset_query_tag

--- macros/query_tags.sql
+++ macros/query_tags.sql
@@ -67,3 +67,17 @@
     {% do run_query("alter session set query_tag = '{}'".format(new_query_tag)) %}
     {{ return(original_query_tag)}}
 {% endmacro %}
+
+{% macro unset_query_tag(original_query_tag) -%}
+    {{ return(adapter.dispatch('unset_query_tag', 'dbt_snowflake_query_tags')(original_query_tag)) }}
+{%- endmacro %}
+
+{% macro default__unset_query_tag(original_query_tag) -%}
+    {% if original_query_tag %}
+    {{ log("Resetting query_tag to '" ~ original_query_tag ~ "'.") }}
+    {% do run_query("alter session set query_tag = '{}'".format(original_query_tag)) %}
+    {% else %}
+    {{ log("No original query_tag, unsetting parameter.") }}
+    {% do run_query("alter session unset query_tag") %}
+    {% endif %}
+{% endmacro %}
