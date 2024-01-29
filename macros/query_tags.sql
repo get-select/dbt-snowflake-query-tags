05fac2c73e57dac5c17416f8018d74ad9ce09ba6
Niall Woodward <niall@niallrees.com>
2024-01-29 10:20:43 +0000

    Changelog

--- macros/query_tags.sql
+++ macros/query_tags.sql
@@ -35,7 +35,7 @@
 
     {%- do query_tag.update(
         app='dbt',
-        dbt_snowflake_query_tags_version='2.3.2',
+        dbt_snowflake_query_tags_version='2.3.3',
     ) -%}
 
     {% if thread_id %}
